bc305cced4a0a110cb5aa42878ce2d697c34ef93
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-11-14 22:26:25 +0700

    menyambungkan backend chatbot ke laravel agar dapat di akses, disini kami menggunakan API dari Gemini untuk chatbotnya.

---
+++ app/Http/Controllers/ChatbotController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ChatbotController extends Controller
+{
+    public function generate(Request $request)
+    {
+        $response = Http::post('http://127.0.0.1:5000/generate', [
+            'user_input' => $request->message
+        ]);
+
+        return response()->json([
+            'response' => $response->json()['response'] ?? $response->body()
+        ]);
+    }
+}
